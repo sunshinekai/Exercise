8c5926e00d55fece61bc4058ef881861c11729d2
张凯 <2748771839@qq.com>
2020-06-20 22:13:52 +0800

    OJ习题之day_14

---
+++ day_14/day_14/day_14.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <cstdio>
+
+using namespace std;
+//
+//int main()
+//{
+//	int n;
+//	vector <int> v;
+//	cin >> n;
+//	v.resize(n);
+//	for (auto &e : v)
+//		cin >> e;
+//	int sum = 1;
+//	int product = 0;
+//	int count = 0;
+//	int i = 0;
+//	while (i < n)
+//	{
+//		sum = 0;
+//		product = 1;
+//		for (auto &i : v)
+//			sum += i;
+//
+//		for (auto &j : v)
+//			product *= j;
+//
+//		if (sum > product)
+//			count++;
+//		v.pop_back();
+//		i++;
+//	}
+//	cout << count << endl;
+//	return 0;
+//}
